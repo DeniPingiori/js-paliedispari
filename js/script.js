2ca0d1cd832e2cf328eda8471e43ec99533a508f
Denise Pingiori <pingiorid@gmail.com>
2024-07-06 13:21:43 +0200

    [upd] dichiaration var and tell function

--- js/script.js
+++ js/script.js
@@ -1,16 +1,16 @@
 // PALIDROMA
 
 //definisco la funzione
-function checkPalindroma(string){
+function checkPalindroma(string) {
 
     //definisco la variabile che conterrà la parola al contrario
     let check_palindroma;
 
     //prendo la parola passata come parametro e la splito, inverto e riunisco i caratteri
-    check_palindroma = string.split('').reverse().join();
+    check_palindroma = string.split('').reverse().join('');
 
     //verifico che il contenuto della var check_palindroma sia = a quello del parametro string
-    if(check_palindroma.toLowerCase() === string.toLowerCase()){
+    if(check_palindroma === string) {
        console.log('la stringa è palindroma'); 
     }
     else{
@@ -19,6 +19,7 @@ function checkPalindroma(string){
 }
 
 //dichiaro la var che contiene la parola inserita dall'user e assegno prompt
-let word = prompt('inserisci la parola da verificare')
+let word = prompt('inserisci la parola da verificare');
 
 // chiamo la funzione
+checkPalindroma(word.toLocaleLowerCase());
